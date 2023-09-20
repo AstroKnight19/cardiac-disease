b2233786e8e971dbb5910c584d0f55cf0308f470
AstroKnight19 <114679517+AstroKnight19@users.noreply.github.com>
2023-09-20 09:37:47 +0530

    Update about.py

--- Cardiac-Arrest-Predictor-master/Cardiac-Arrest-Predictor-master/Tabs/about.py
+++ Cardiac-Arrest-Predictor-master/Cardiac-Arrest-Predictor-master/Tabs/about.py
@@ -1,16 +1,1 @@
-"""This modules contains data about about page"""
 
-# Import necessary modules
-import streamlit as st
-from PIL import Image
-
-
-def app():
-    """This function creates the about page"""
-    st.balloons()
-    st.title('Contact Us')
-    st.markdown('''### Name:
-    Mainak Chaudhuri''')
-    st.image("E:\CAP\Cardiac-Arrest-Predictor-master\Cardiac-Arrest-Predictor-master\images\icon.jpg")
-    st.markdown('''### GitHub: [Mainak](https://github.com/MainakRepositor/)''')
-    
